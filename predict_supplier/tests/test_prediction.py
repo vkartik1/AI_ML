84f942dfd8d5c5b57b6bd1301a37f8f50a78e6f6
vkartik1 <vkartik@gmail.com>
2020-09-18 14:13:25 -0700

    test checks for threshold of probability

--- predict_supplier/tests/test_prediction.py
+++ predict_supplier/tests/test_prediction.py
@@ -25,19 +25,18 @@ def test_single_api_call():
     expected_response = [[{'supplier': 'REI', 'probability': 0.37}, {'supplier': 'Olympia Sporting Goods', 'probability': 0.26}]]
 
     with app.test_client() as client:
-        print('start')
         # Test client uses "query_string" instead of "params"
         response = client.get('/api/v1/sourcing/auction', query_string=data)
-        print(response)
-        # Check that we got "200 OK" back.
         assert response.status_code == 200
+        data = json.loads(response.data)
+        print(data)
+        assert (data[0][0]["supplier"]) == 'REI'
+        assert (data[0][0]["probability"] >= 0.30 )
+        assert (data[0][1]["supplier"] == 'Olympia Sporting Goods')
+        assert (data[0][1]["probability"] >= 0.20)
+     
         # response.data returns bytes, convert to a dict.
-        assert json.loads(response.data) == expected_response
+        # assert json.loads(response.data) == expected_response
 
 #if __name__ == '__main__':
-#    ItemSelector.__module__ = "model_supplier"
-#    CategorySelector.__module__ = "model_supplier"
-#    NumberSelector.__module__ = "model_supplier"
-
-if __name__ == '__main__':
-    app.run(debug=True)
+#    app.run(debug=True)
